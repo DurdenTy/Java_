a0f6ccd6eea0e216104a4a236dae73ab8b7564ce
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-08-06 19:06:45 -0300

    Create Fila.h

---
+++ Fila, Pilha e Lista encadeada/Fila.h
@@ -0,0 +1,51 @@
+public class Fila<T> {
+
+    private No inicio;
+    private No fim;
+    private int idx = 0;
+
+    public Fila(){
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    public void enfileira(T dado){
+
+        No enfileira = new No();
+        enfileira.setDado(dado);
+        enfileira.setProx(null);
+            if(inicio == null){
+                inicio = enfileira;
+            }else{
+                fim.setProx(enfileira);
+            }
+            fim = enfileira;
+            idx++;
+    }
+
+    public T desenfileira(){
+        if(cheia()){
+            No tmp = inicio;
+            inicio = (inicio.getProx());
+            T valor = (T)tmp.getDado();
+            idx--;
+            return valor;
+        }else{
+           System.out.println("Fila vazia!");
+           return null;
+        }
+
+    }
+
+    public boolean cheia(){
+        return inicio != null ? true : false;
+    }
+
+    public boolean vazia(){
+        return inicio == null ? true : false;
+    }
+
+    public int tamanho(){
+        return idx;
+    }
+}
